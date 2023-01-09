6dba6ac4969eb8da2c3a9b4312b30fb6fadd0ff7
何佳 <jia.he@yottacloud.cn>
2023-01-09 18:20:15 +0800

    [*]优化了一个输入框的bug

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,68 +1,78 @@
 <template>
   <div id="app">
-    <h2>输出sm4(ouput base64格式)的加密</h2>
-    <div>JavaScript加密代码示范：</div>
-    <code>
-      import { SM4 } from "gm-crypto"; <br />
-      var val = SM4.encrypt(raw, this.val, {<br />
-      inputEncoding: "utf8",<br />
-      outputEncoding: "base64",<br />
-      });<br />
-    </code>
-    <br />
-    <br />
-    <div>
-      密钥
+    <div class="blockcenter">
+      <h2 class="">SM4(BASE64)加解密工具</h2>
+      <div class="">JavaScript加密代码示范：</div>
+      <code>
+        import { SM4 } from "gm-crypto"; <br />
+        var val = SM4.encrypt(raw, this.val, {<br />
+        inputEncoding: "utf8",<br />
+        outputEncoding: "base64",<br />
+        });<br />
+      </code>
+      <br />
+      <br />
+      <div>
+        密钥
+        <input
+          type="checkbox"
+          label="保存密码"
+          :checked="isSave"
+          name="save_s"
+          @change="checked"
+        /><label for="save_s">保存密钥</label>
+      </div>
       <input
-        type="checkbox"
-        label="保存密码"
-        :checked="isSave"
-        name="save_s"
-        @change="checked"
-      /><label for="save_s">保存密钥</label>
+        v-model="val"
+        type="password"
+        oncopy="return false"
+        placeholder="请输入密钥"
+        @change="keyChange"
+      />
+      <div>加密/解密字符</div>
+      <input v-model="val2" type="text" placeholder="请输入字符" />
+      <br />
+      <br />
+      <div>
+        结果：<span>{{ val3 || '暂无结果' }}</span>&nbsp;&nbsp;
+        <button @click="copy" v-if="val3">复制</button>
+      </div>
+      <div style="margin-top: 5px">
+        <button @click="encryptSensitiveData(val2)">加密</button>
+        &nbsp; &nbsp; &nbsp;
+        <button @click="decryptSensitiveData(val2)">解密</button>
+      </div>
+      <br />
+      <div>密钥生成</div>
+      <button @click="createKey">点击随机生成</button>
+      <div v-if="key">
+        {{ key }} <button @click="copyRandomKey">复制</button>
+      </div>
     </div>
-    <input
-      v-model="val"
-      type="password"
-      oncopy="return false"
-      placeholder="请输入密钥"
-      @change="keyChange"
-    />
-    <div>加密/解密字符</div>
-    <input v-model="val2" type="text" placeholder="请输入字符" />
-    <br />
-    <br />
-    <div>
-      结果:<span>{{ val3 }}</span> <button @click="copy" v-if="val3">复制</button>
-    </div>
-    <div>
-      <button @click="encryptSensitiveData(val2)">加密</button>
-      <button @click="decryptSensitiveData(val2)">解密</button>
-    </div>
-    <br />
-    <div>密钥生成</div>
-    <button @click="createKey">点击随机生成</button>
-    <div v-if="key">{{ key }} <button @click="copyRandomKey">复制</button></div>
   </div>
 </template>
 
 <script>
 import { SM4 } from "gm-crypto";
 let clipboardWrite = () => {
-  alert("请yarn dev 在electron环境下操作");
+  Toast("请yarn dev 在electron环境下操作");
 };
 
-function Toast(msg,duration){
-  duration=isNaN(duration)?3000:duration;
-  var m = document.createElement('div');
+function Toast(msg, duration) {
+  duration = isNaN(duration) ? 3000 : duration;
+  var m = document.createElement("div");
   m.innerHTML = msg;
-  m.style.cssText="max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
+  m.style.cssText =
+    "max-width:80%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
   document.body.appendChild(m);
-  setTimeout(function() {
+  setTimeout(function () {
     var d = 0.5;
-    m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
-    m.style.opacity = '0';
-    setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+    m.style.webkitTransition =
+      "-webkit-transform " + d + "s ease-in, opacity " + d + "s ease-in";
+    m.style.opacity = "0";
+    setTimeout(function () {
+      document.body.removeChild(m);
+    }, d * 1000);
   }, duration);
 }
 
@@ -93,23 +103,31 @@ export default {
       clipboardWrite(this.val3);
     },
     encryptSensitiveData(raw) {
+      if (!raw) {
+        Toast("请输入加解密字符", 1000);
+        return;
+      }
       try {
         this.val3 = SM4.encrypt(raw, this.val, {
           inputEncoding: "utf8",
           outputEncoding: "base64",
         });
       } catch (e) {
-        alert("加密出错，请输入正确的密钥和字符串");
+        Toast("加密出错，请输入正确的密钥字符串", 1000);
       }
     },
     decryptSensitiveData(raw) {
+      if (!raw) {
+        Toast("请输入加解密字符", 1000);
+        return;
+      }
       try {
         this.val3 = SM4.decrypt(raw, this.val, {
           inputEncoding: "base64",
           outputEncoding: "utf8",
         });
       } catch (e) {
-        alert("解密出错，请输入正确的密钥和加密字符串");
+        Toast("解密出错，请输入正确的密钥字符串", 1000);
       }
     },
 
@@ -146,4 +164,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
+#app{
+  display: flex;
+  justify-content: center;
+}
+.blockcenter{
+  width: 350px;
+}
+.center {
+  text-align: center;
+}
+.center-code {
+  display: block;
+  margin: 20px auto;
+  width: 300px;
+}
 </style>
